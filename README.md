08eacef7aadae6acdc5b376743d5cc877a555484
hsdistefa <hsdistefa@gmail.com>
2021-04-14 23:29:23 -0700

    Created simple NFT

---
+++ README.md
@@ -0,0 +1,4 @@
+# NFT
+Basic NFT for learning purposes
+
+Based on Chainlink truffle box
---
+++ contracts/NFTSimple.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.6.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract NFTSimple is ERC721 {
+    constructor() ERC721("MyNFT", "MNFT") public {
+
+    }
+}
